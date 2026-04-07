0d012a57836ed6931ae7062f52e90d1e2186e606
Zzynox1 <a7752988@gmail.com>
2026-04-07 15:23:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
 const { Client } = require('discord.js-selfbot-v13');
-const { joinVoiceChannel } = require("@discordjs/voice");
-
+const { joinVoiceChannel } = require('@discordjs/voice');
 const client = new Client({ checkUpdate: false });
-
-const config = require(`${process.cwd()}/config.json`);
+const config = require('./config.json');
 
 client.on('ready', async () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-
-    await joinVC(client, config);
+  console.log(`Logged in as ${client.user.tag}!`);
+  await joinVC(client, config);
 });
 
 client.on('voiceStateUpdate', async (oldState, newState) => {
-    const oldVoice = oldState.channelId;
-    const newVoice = newState.channelId;
+  const oldVoice = oldState.channelId;
+  const newVoice = newState.channelId;
 
-    if (oldVoice !== newVoice) {
-        if (!oldVoice) {
-            // empty
-        } else if (!newVoice) {
-            if (oldState.member.id !== client.user.id) return;
-            await joinVC(client, config);
-        } else {
-            if (oldState.member.id !== client.user.id) return;
-            if (newVoice !== config.Channel) {
-                await joinVC(client, config);
-            }
-        }
+  if (oldVoice !== newVoice) {
+    if (!newVoice) {
+      // Re-join if kicked or disconnected
+      if (oldState.member.id !== client.user.id) return;
+      await joinVC(client, config);
+    } else {
+      // Ensure we stay in the target channel
+      if (oldState.member.id !== client.user.id) return;
+      if (newVoice !== config.Channel) {
+         await joinVC(client, config);
+      }
     }
+  }
 });
-client.login(config.Token);
-// Copyright by sivvv0
+
+// This line checks Railway's "Variables" first, then the config file
+client.login(process.env.TOKEN || config.Token);
+
 async function joinVC(client, config) {
-    const guild = client.guilds.cache.get(config.Guild);
-    const voiceChannel = guild.channels.cache.get(config.Channel);
-    const connection = joinVoiceChannel({
-        channelId: voiceChannel.id,
-        guildId: guild.id,
-        adapterCreator: guild.voiceAdapterCreator,
-        selfDeaf: false,
-        selfMute: true
-    });
+  const guildId = config.Guild;
+  const guild = client.guilds.cache.get(guildId);
+  const voiceChannel = guild.channels.cache.get(config.Channel || process.env.CHANNEL);
+  
+  const connection = joinVoiceChannel({
+    channelId: voiceChannel.id,
+    guildId: guild.id,
+    adapterCreator: guild.voiceAdapterCreator,
+    selfDeaf: false,
+    selfMute: true
+  });
 }
